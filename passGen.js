1e22e640d39d6a8654a5d4d3b01e1b3e43e60fd9
EliteSamurai <86984995+EliteSamurai@users.noreply.github.com>
2023-03-05 22:52:28 -0600

    Add password length option and clean up the CSS

--- passGen.js
+++ passGen.js
@@ -1,19 +1,19 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let firstPass = document.getElementById("first")
-let secondPass = document.getElementById("second")
-let passwordLength = 15
+const firstPass = document.getElementById("first")
+const secondPass = document.getElementById("second")
+const passwordLength = document.getElementById('num')
 
 function generatePass() {
-    let randomize = Math.floor(Math.random() * characters.length)
+    const randomize = Math.floor(Math.random() * characters.length)
     return characters[randomize]
 }
 
 function randomPass() {
     let password1 = ""
     let password2 = ""
-    for (let i = 0; i < passwordLength; i++) {
+    for (let i = 0; i < passwordLength.value; i++) {
         password1 += generatePass()
         password2 += generatePass()
     }
